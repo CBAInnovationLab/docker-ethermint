a068beba0c33a5da8e42367748397c6d042d022d
Dan Turner <dan.turner@cba.com.au>
2017-12-04 16:14:51 +1100

    Allow path to a genesis file to be specified by ETH_GENESIS_FILE environment variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-if [ -n "$ETH_GENESIS_CONTENT" ] && [ ! -f ~/ethereum-genesis.json ]; then
-    echo "--> Writing Genesis to ~/ethereum-genesis.json"
-    echo $ETH_GENESIS_CONTENT > ~/ethereum-genesis.json
+if [ -z "$ETH_GENESIS_FILE" ]; then
+  export ETH_GENESIS_FILE=~/ethereum-genesis.json
+fi
+
+if [ -n "$ETH_GENESIS_CONTENT" ] && [ ! -f "$ETH_GENESIS_FILE" ]; then
+    echo "--> Writing Genesis to $ETH_GENESIS_FILE"
+    echo $ETH_GENESIS_CONTENT > $ETH_GENESIS_FILE
 fi
 
 if [ ! -d ~/.ethereum/ethermint/chaindata ]; then
-    echo "--> Initialising Blockchain"
-    if [ ! -f ~/ethereum-genesis.json ]; then
-      echo "Missing Ethereum genesis file at ~/ethereum-genesis.json"
+    echo "--> Initialising Blockchain using: $ETH_GENESIS_FILE"
+    if [ ! -f $ETH_GENESIS_FILE ]; then
+      echo "Missing Ethereum genesis file at $ETH_GENESIS_FILE"
       exit 1
     fi
-    ethermint --with-tendermint init ~/ethereum-genesis.json
+    ethermint --with-tendermint init $ETH_GENESIS_FILE
 fi
 
 echo "--> Starting Ethermint"
